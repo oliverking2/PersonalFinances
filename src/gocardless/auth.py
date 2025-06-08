4cb33b26b83ea42ebd349c608acbd1baebb2dfaf
Oliver <oliver@oliverking.me.uk>
2025-06-08 19:06:35 +0100

    Add token-based authentication to GoCardless credentials module.

--- src/gocardless/auth.py
+++ src/gocardless/auth.py
@@ -1,8 +1,15 @@
 """Module containing the Auth for GoCardless."""
 
 import os
+import time
+from typing import Optional
 
-import dotenv
+from dotenv import load_dotenv
+import requests
+
+
+class GoCardlessError(Exception):
+    """GoCardless Error Class."""
 
 
 class GoCardlessCredentials:
@@ -10,7 +17,57 @@ class GoCardlessCredentials:
 
     def __init__(self) -> None:
         """Initialise the credentials class."""
-        dotenv.load_dotenv()
+        load_dotenv()
 
-        self.client_id = os.getenv("CLIENT_ID")
-        self.client_secret = os.getenv("CLIENT_SECRET")
+        secret_id = os.getenv("SECRET_ID")
+        secret_key = os.getenv("SECRET_KEY")
+
+        if secret_id is None:
+            raise EnvironmentError("SECRET_ID not set.")
+        if secret_key is None:
+            raise EnvironmentError("SECRET_KEY not set.")
+
+        self.secret_id: str = secret_id
+        self.secret_key: str = secret_key
+
+        self._access_token: Optional[str] = None
+        self.access_token_expiry: Optional[float] = None
+
+    def get_access_token(self) -> str:
+        """Get the access token."""
+        json = {
+            "secret_id": self.secret_id,
+            "secret_key": self.secret_key,
+        }
+        response = requests.post(
+            "https://bankaccountdata.gocardless.com/api/v2/token/new/", data=json
+        )
+        response.raise_for_status()
+
+        response_json = response.json()
+
+        self._access_token = response_json["access"]
+        self.access_token_expiry = time.time() + response_json["access_expires"] - 10
+
+        return self._access_token
+
+    @property
+    def access_token(self) -> str:
+        """Get the access token."""
+        if self.access_token_expiry is None:
+            # initial run, get a token
+            return self.get_access_token()
+
+        if time.time() > self.access_token_expiry:
+            # if expired, get a new one
+            return self.get_access_token()
+
+        if self._access_token is not None:
+            return self._access_token
+
+        raise ValueError("Access Token Failure.")
+
+
+if __name__ == "__main__":
+    creds = GoCardlessCredentials()
+    print(creds.access_token)
